cb9e3c75fa9e1a886e77be6365f34d8b4693d9a5
Matan Mashraki <mail@matanmashraki.com>
2019-06-30 22:32:38 +0300

    Initial Commit

---
+++ VSCode/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VSCode
+//
+//  Created by Matan Mashraki on 30/06/2019.
+//  Copyright © 2019 Matan Mashraki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
